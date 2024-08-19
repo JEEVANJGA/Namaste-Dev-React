57d6fd786b44921a671fc02a9ff2d93339292fd9
Jeevan George Antony <antony.jeevangeorge@gmail.com>
2024-08-19 18:50:37 +0530

    refactor: Update app.js to use nested React.createElement calls for creating parent and child elements

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
-const heading = React.createElement(
-    "h1", // tag
+/**
+ * 
+ * <div id="parent">
+ *      <div id="child">
+ *         <h1>I'm  H1 tag  </h1>
+ *      </div>
+ * </div>
+ */
+
+const parent = React.createElement(
+    "div",
     {
-        id: 'heading',
-        xyz: 'abc'
-    }, // props
-    "Hello World from React!" // children
+        id: 'parent'
+    },
+    React.createElement(
+        "div",
+        {
+            id: 'child'
+        },
+        React.createElement(
+            "h1",
+            null,
+            "I'm  H1 tag"
+        )
+    )
 );
 
-console.log(heading);
+console.log(parent); // object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
+
+/******* */
+// const heading = React.createElement(
+//     "h1", // tag
+//     {
+//         id: 'heading',
+//         xyz: 'abc'
+//     }, // props
+//     "Hello World from React!" // children
+// );
+
+// console.log(heading); // object
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);

4a3062843364c26c848da6f7e3e671b74cfd9140
Jeevan George Antony <antony.jeevangeorge@gmail.com>
2024-09-23 22:52:59 +0530

    Chapter-4 | Mock Designing

---
+++ Notes/chapter-4.md
@@ -0,0 +1,33 @@
+
+# Plan - the app to develop
+
+1. App to develop - Food ordering App
+2. App Name - ?
+3. Food App High Level Structure
+   1. APP
+      1. Header
+         1. Logo on left
+         2. Routes on right - Home, About, etc
+      2. Body
+         1. Search bar with search button
+         2. Reastaurant cards
+            1. Restaurant image
+            2. Restaurant name
+            3. cusine tags
+            4. rating
+      3. Footer
+         1. Copyright
+         2. Links
+4. Component List
+   1. Header
+      1. Logo
+      2. nav bar
+   2. Body
+      1. Search
+      2. RestaurantContainer
+         1. RestaurantCard
+   3. Footer
+      1. CopyRight
+      2. Links
+      3. Address
+      4. Contact
--- app.js
+++ app.js
@@ -1,60 +1,106 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import Header from './components/header/header';
+/******* */
+/** Chapter-4 */
+/**
+ * Component List
+   1. Header
+      1. Logo
+      2. nav bar
+   2. Body
+      1. Search
+      2. RestaurantContainer
+         1. RestaurantCard
+   3. Footer
+      1. CopyRight
+      2. Links
+      3. Address
+      4. Contact
+ */
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='logo-container'>
+                <img className='logo' src='https://png.pngtree.com/png-vector/20221231/ourmid/pngtree-food-delivery-logo-template-design-sign-menu-vector-png-image_43664212.jpg' alt='logo' />
+            </div>
+            <div className='navItems'>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+const AppLayout = () => {
+    return (
+        <div className='app'>
+            <Header />
+        </div>
+    );
+};
+
+const root = createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
 
 /******* */
 /** Chapter-3 */
+// import Header from './components/header/header';
 
-//  React.createElement ==> ReactElement-JS Object ==> HTMLElement(render)
-// * React Element
-// const heading = React.createElement(
-//     "h1", // tag
-//     {
-//         id: 'heading',
-//         xyz: 'abc'
-//     }, // props
-//     "Hello World from React!" // children
+// //  React.createElement ==> ReactElement-JS Object ==> HTMLElement(render)
+// // * React Element
+// // const heading = React.createElement(
+// //     "h1", // tag
+// //     {
+// //         id: 'heading',
+// //         xyz: 'abc'
+// //     }, // props
+// //     "Hello World from React!" // children
+// // );
+
+// // JSX - HTML-like or XML-like syntax
+// //  JSX ==> Babel transpiles to React.createElement ==> ReactElement-JS Object ==> HTMLElement(render)
+// const jsXHeading = (
+//     <h1 id="heading" className="abc" tabIndex={1}>
+//         React - element | using JSX!
+//     </h1>
 // );
 
-// JSX - HTML-like or XML-like syntax
-//  JSX ==> Babel transpiles to React.createElement ==> ReactElement-JS Object ==> HTMLElement(render)
-const jsXHeading = (
-    <h1 id="heading" className="abc" tabIndex={1}>
-        React - element | using JSX!
-    </h1>
-);
+// // * React Component
+// // * React Functional Component
+// // const HeadingComponent = () => {
+// //     return <h1 id="heading" className="abc" tabIndex={1}>Hello World from React - using JSX!</h1>;
+// // }
 
-// * React Component
-// * React Functional Component
-// const HeadingComponent = () => {
-//     return <h1 id="heading" className="abc" tabIndex={1}>Hello World from React - using JSX!</h1>;
-// }
+// const Title = () => (
+//     <h1 id="title">I'm a title</h1>
+// );
 
-const Title = () => (
-    <h1 id="title">I'm a title</h1>
-);
-
-// * React Component Composition
-const HeadingComponent = () => (
-    <div id='container'>
-        <Header />
-        {Title()}
-        <Title />
-        <Title></Title>
-        {jsXHeading}
-        <h1 id="heading" className="abc" tabIndex={1}>
-            Hello World from React - using functional component
-        </h1>
-    </div>
-);
+// // * React Component Composition
+// const HeadingComponent = () => (
+//     <div id='container'>
+//         <Header />
+//         {Title()}
+//         <Title />
+//         <Title></Title>
+//         {jsXHeading}
+//         <h1 id="heading" className="abc" tabIndex={1}>
+//             Hello World from React - using functional component
+//         </h1>
+//     </div>
+// );
 
 
 
-const root = createRoot(document.getElementById("root"));
+// const root = createRoot(document.getElementById("root"));
 
-// root.render(heading);
-root.render(<HeadingComponent />);
+// // root.render(heading);
+// root.render(<HeadingComponent />);
 
 /******* */
 /** Chapter-2 */

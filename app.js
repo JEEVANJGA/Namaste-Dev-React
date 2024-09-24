91e4a60ce053642b8dc5b1a5bd6cb1bdb82abc76
Jeevan George Antony <antony.jeevangeorge@gmail.com>
2024-09-24 22:54:25 +0530

    Chapter-4 | CSS - 05 | Add multiple RestaurantCard components and update res-container styles

--- app.js
+++ app.js
@@ -64,6 +64,13 @@ const Body = () => {
             <div className='search'> Search</div>
             <div className='res-container'>
                 <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
             </div>
         </div>
     )

cce966ace0eb179a85ce17330181fe818bfbb437
Jeevan George Antony <antony.jeevangeorge@gmail.com>
2024-11-01 12:09:43 +0530

    Chapter-5 | Implement restaurant filtering functionality in Body component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,33 @@
+import { useState } from 'react';
 
 import RestaurantCard from './RestaurantCard';
 
 import { restarantList } from '../utils/mockData';
 
 const Body = () => {
+
+    const [listOfRestaurants, setListOfRestaurants] = useState(restarantList);
+
     return (
         <div className='Body'>
-            <div className='search'> Search</div>
+            <div className='filter'>
+                <button
+                    className='filter-btn'
+                    onClick={() => {
+                        const filteredList = restarantList.filter((restaurant) => restaurant.info.avgRating > 4.3);
+                        setListOfRestaurants(filteredList);
+                    }}
+                >Top Rated Restaurants </button>
+                <button
+                    className='filter-btn'
+                    onClick={() => {
+                        setListOfRestaurants(restarantList);
+                    }}
+                >Reset</button>
+            </div>
             <div className='res-container'>
                 {
-                    restarantList?.map((restaurant) => {
+                    listOfRestaurants?.map((restaurant) => {
                         return <RestaurantCard key={restaurant.info.id} restaurantDetails={restaurant} />
                     })
                 }

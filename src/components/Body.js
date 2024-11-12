fe94f043a70cbcacb980f5de3ea457872cdf39b8
Jeevan George Antony <antony.jeevangeorge@gmail.com>
2024-11-12 10:24:09 +0530

    Chapter-6 | Enhance Body component with search functionality and UI improvements

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,25 +1,32 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import RestaurantCard from './RestaurantCard';
-import Shimmer from './Shimmer';
+import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
 
 // import { restarantList } from '../utils/mockData';
 
 const Body = () => {
 
+    // Whenever state variable gets updated, react triggers a reconciliation process ( re-rendering of the component )
+
     const [listOfRestaurants, setListOfRestaurants] = useState(null);
     const [copyOfRestaurants, setCopyOfRestaurants] = useState(null);
 
+    const [searchText, setSearchText] = useState("");
+
     const fetchData = async () => {
         try {
-            const response = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0615195&lng=77.6163803&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+            const response = await fetch(
+                "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0615195&lng=77.6163803&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+            );
             const data = await response.json();
-            const restaurant_list = await data?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+            const restaurant_list = await data?.data?.cards?.[4]?.card?.card
+                ?.gridElements?.infoWithStyle?.restaurants;
             console.log(data, restaurant_list);
             setCopyOfRestaurants(restaurant_list);
             setListOfRestaurants(restaurant_list);
         } catch (error) {
-            console.log('Error fetching data: ', error);
+            console.log("Error fetching data: ", error);
         }
     };
 
@@ -33,34 +40,70 @@ const Body = () => {
     //     return <Shimmer />;
     // }
 
-    return (!listOfRestaurants || listOfRestaurants?.length === 0) ?
+    return !listOfRestaurants || listOfRestaurants?.length === 0 ? (
         <Shimmer />
-        : (
-            <div className='Body'>
-                <div className='filter'>
+    ) : (
+        <div className="Body">
+            <div className="filter">
+                <div className="search">
+                    <input
+                        type="text"
+                        placeholder="Search for restaurants"
+                        className="search-box"
+                        value={searchText}
+                        onChange={(e) => setSearchText(e.target.value)}
+                    />
                     <button
-                        className='filter-btn'
+                        className="search-btn"
                         onClick={() => {
-                            const filteredList = copyOfRestaurants.filter((restaurant) => restaurant.info.avgRating > 4.3);
+                            // Filter the restaurant cards based on the search input and update the UI
+                            // searchText = input value
+                            const filteredList = copyOfRestaurants.filter((restaurant) =>
+                                restaurant.info.name
+                                    .toLowerCase()
+                                    .includes(searchText.toLowerCase())
+                            );
                             setListOfRestaurants(filteredList);
                         }}
-                    >Top Rated Restaurants </button>
+                    >
+                        Search
+                    </button>
+                </div>
+                <div>
                     <button
-                        className='filter-btn'
+                        className="filter-btn"
+                        onClick={() => {
+                            const filteredList = copyOfRestaurants.filter(
+                                (restaurant) => restaurant.info.avgRating > 4.3
+                            );
+                            setListOfRestaurants(filteredList);
+                        }}
+                    >
+                        Top Rated Restaurants{" "}
+                    </button>
+                    <button
+                        className="filter-btn"
                         onClick={() => {
                             setListOfRestaurants(copyOfRestaurants);
+                            setSearchText(""); // Reset the search input
                         }}
-                    >Reset</button>
-                </div>
-                <div className='res-container'>
-                    {
-                        listOfRestaurants?.map((restaurant) => {
-                            return <RestaurantCard key={restaurant.info.id} restaurantDetails={restaurant} />
-                        })
-                    }
+                    >
+                        Reset
+                    </button>
                 </div>
             </div>
-        )
+            <div className="res-container">
+                {listOfRestaurants?.map((restaurant) => {
+                    return (
+                        <RestaurantCard
+                            key={restaurant.info.id}
+                            restaurantDetails={restaurant}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
 };
 
 export default Body;

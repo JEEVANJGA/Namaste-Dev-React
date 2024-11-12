ab52ed6eebdb9c0a41532951d8c3d9e0b30d86b6
Jeevan George Antony <antony.jeevangeorge@gmail.com>
2024-11-12 22:41:00 +0530

    Chapter-6.2 | Update API endpoint in Body component to use CORS proxy for restaurant data fetching

--- src/components/Body.js
+++ src/components/Body.js
@@ -17,7 +17,7 @@ const Body = () => {
     const fetchData = async () => {
         try {
             const response = await fetch(
-                "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0615195&lng=77.6163803&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+                "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0615195&lng=77.6163803&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
             );
             const data = await response.json();
             const restaurant_list = await data?.data?.cards?.[4]?.card?.card
